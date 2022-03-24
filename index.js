2dabc5ec0373633a362d402cda5770cebcb9adee
codebeakers <emprendetufutoro8@gmail.com>
2022-03-24 12:20:57 -0500

    Se agrega l proyecto

---
+++ index.js
@@ -0,0 +1,31 @@
+// alert("Hola como estas?");
+
+function limpiar(){
+    document.getElementsById('miFormulario').reset();
+}
+
+function sumar  () { 
+    var x = parseInt(document.getElementById('valor 1').value);
+    var y = parseInt(document.getElementById('valor 2').value);
+    document.getElementById('el-resultado').innerHTML = x+y;
+}
+
+function restar  () { 
+    var x = parseInt(document.getElementById('valor 1').value);
+    var y = parseInt(document.getElementById('valor 2').value);
+    document.getElementById('el-resultado').innerHTML = x-y;
+}
+
+function multiplicar  () { 
+    var x = parseInt(document.getElementById('valor 1').value);
+    var y = parseInt(document.getElementById('valor 2').value);
+    document.getElementById('el-resultado').innerHTML = x*y;
+}
+
+function dividir  () { 
+    var x = parseInt(document.getElementById('valor 1').value);
+    var y = parseInt(document.getElementById('valor 2').value);
+    document.getElementById('el-resultado').innerHTML = x/y;
+}
+
+
